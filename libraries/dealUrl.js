57bb27178af9d06b9c947bc2575a1f74d49917ea
havenxie <1428876581@qq.com>
2017-04-11 18:49:43 +0800

    add animate to top

--- libraries/dealUrl.js
+++ libraries/dealUrl.js
@@ -4,6 +4,9 @@ module.exports = {
   getUrlTypeId(option) {
         let url = option.currentTarget.dataset.id;//注意这里是小写
         let apiLength = (url.indexOf(newsdata.API_URL) != -1) ? newsdata.API_URL.length : newsdata.API_URL2.length;
+        if(url.indexOf('https') == -1) {//是http而不是https
+            apiLength--;
+        }
         let indexOfId = url.indexOf('?');
         let urlId = url.substr(indexOfId + 1);
         let urlType = url.substr(apiLength + 1, indexOfId - apiLength -1);
